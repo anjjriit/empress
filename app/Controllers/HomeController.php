0c668171209ee0209f35b8b4183633dcce711060
Vince Kronlein <vkronlein@icloud.com>
2017-02-07 02:43:51 -0700

    migration changes for utf8mb4
    Signed-off-by: Vince Kronlein <vkronlein@icloud.com>

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * app/Controllers/HomeController.php
+ *
+ * Resourceful controller for the homepage.
+ *
+ * @author Vince Kronlein <vince@19peaches.com>
+ * @license https://github.com/19peaches/empress/blob/master/LICENSE
+ * @copyright Periapt, LLC. All Rights Reserved.
+ */
+
+namespace Empress\Controllers;
+
+use Empress\Base\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index ()
+    {
+    	
+    }
+}
--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -22,8 +22,8 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token')->index();
+            $table->string('email', 180)->index();
+            $table->string('token', 180)->index();
             $table->timestamp('created_at')->nullable();
         });
     }
