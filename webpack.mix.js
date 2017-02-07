e9aece11b54ac7774aeb75be97c3d08323252b46
Vince Kronlein <vkronlein@icloud.com>
2017-02-07 07:37:11 -0700

    added versioning
    Signed-off-by: Vince Kronlein <vkronlein@icloud.com>

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ const { mix } = require('laravel-mix');
 
 mix.copy('node_modules/material-design-icons/iconfont', 'resources/assets/sass/fonts')
    .js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .version();
